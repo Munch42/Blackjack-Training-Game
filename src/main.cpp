d0dffa700aed7ce901266d0f5e9e9914ac8537fa
Munch42 <munch4242@gmail.com>
2024-11-15 15:56:10 -0700

    Testing SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    auto window = sf::RenderWindow({1920u, 1080u}, "CMake SFML Project");
+    auto window = sf::RenderWindow({1920u, 1080u}, "Blackjack Training Game");
     window.setFramerateLimit(144);
 
     while (window.isOpen())
@@ -12,6 +13,23 @@ int main()
             if (event.type == sf::Event::Closed)
             {
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                if (event.key.scancode == sf::Keyboard::Scan::Escape)
+                {
+                    std::cout << "the escape key was pressed" << std::endl;
+                    std::cout << "scancode: " << event.key.scancode << std::endl;
+                    std::cout << "code: " << event.key.code << std::endl;
+                    std::cout << "control: " << event.key.control << std::endl;
+                    std::cout << "alt: " << event.key.alt << std::endl;
+                    std::cout << "shift: " << event.key.shift << std::endl;
+                    std::cout << "system: " << event.key.system << std::endl;
+                    std::cout << "description: " << sf::Keyboard::getDescription(event.key.scancode).toAnsiString() << std::endl;
+                    std::cout << "localize: " << sf::Keyboard::localize(event.key.scancode) << std::endl;
+                    std::cout << "delocalize: " << sf::Keyboard::delocalize(event.key.code) << std::endl;
+                }
             }
         }
 
